9393251dc43fcf2fddd36afee9652eb870a5fdeb
nicolefb <nicolefbailey@hotmail.com>
2024-09-30 15:27:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
-        const images = document.querySelectorAll("img");
+//         const images = document.querySelectorAll("img");
 
-        for(const image of images){
-            fetch("https://dog.ceo/api/breeds/image/random")
-            .then(response => response.json())
-            .then(data => {
-                image.src = data.message
-                image.width = 100;
-                image.height = 100;
-        })
-    }
-})
+//         for(const image of images){
+//             fetch("https://dog.ceo/api/breeds/image/random")
+//             .then(response => response.json())
+//             .then(data => {
+//                 image.src = data.message
+//                 image.width = 100;
+//                 image.height = 100;
+//         })
+//     }
+// })
